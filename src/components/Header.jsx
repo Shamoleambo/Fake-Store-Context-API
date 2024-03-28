1646faf141c3c13676c7d2b182e60385a5ba5fc3
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-03-28 14:39:51 -0300

    wrapping my head around vim using this react project as practice. Hard Header to do

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import classes from './Header.module.css'
+
+export function Header (){
+	return <header className={classes.header}>
+		<p className={classes.paragraph}>Fake Store</p>
+		<div className={classes.cart}>cart icon</div>
+		</header>
+}
